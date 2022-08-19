d28a1883c7cc711548c198ca92ff43a5f578f1f1
coeit <cbeiteneuer@gmail.com>
2022-08-19 15:18:33 +0200

    fix: session expiration due to ill timed session polling refactor: add possibility to use AUTH URL instead of ISSUER

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,7 +10,15 @@ import {BASEPATH} from '../config/globals';
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
 
 	return (
-		<SessionProvider session={session} refetchInterval={9.5 * 60} basePath={ `${BASEPATH}/api/auth` }>
+		<SessionProvider
+      session={session}
+      // refetch every 4min 59sec to avoid the session expiring. This interval
+      // has to be smaller than the time difference for a token to count as
+      // "expired" (default 5 min). See [...nextauth].ts
+      refetchInterval={4 * 60 - 1}
+      refetchOnWindowFocus={true}
+      basePath={BASEPATH ? `${BASEPATH}/api/auth` : undefined}
+    >
 			<ThemeProvider theme={theme}>
 				<CssBaseline/>
 				<Component {...pageProps} />
